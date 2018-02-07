97aebea1ad358e498dafa8f20dcfc0749dcba5ca
pli <philippe.libat@free.fr>
2018-02-07 22:14:05 +0100

    Add update projects

--- tools/gitlab.sh
+++ tools/gitlab.sh
@@ -9,5 +9,7 @@ ANSIBLE_ROLES_PATH=$(pwd)/.. ansible-playbook \
     -i tests/hostfile \
     -e "role_name=ansible-gitlab-config" \
     -e "gitlab_host=$GITLAB_HOST" \
+    -e "gitlab_user=$GITLAB_USER" \
+    -e "gitlab_password=$GITLAB_PASSWORD" \
     -e "gitlab_token=$GITLAB_TOKEN" \
     tests/gitlab-config.yml -vv
