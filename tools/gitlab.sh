25eeea94c228ed624970c15151d78ab94257f9f2
pli <philippe.libat@free.fr>
2018-02-08 01:37:48 +0100

    Add members

--- tools/gitlab.sh
+++ tools/gitlab.sh
@@ -3,8 +3,30 @@
 # testing script:
 #  source your variables before executing
 #
-
+set -e
+(
 cd $(dirname $0)/..
+
+echo "# ansible-lint"
+find tasks/ -type f -name "*.yml" -exec ansible-lint {} \;
+
+echo "# ansible check"
+ANSIBLE_ROLES_PATH=$(pwd)/.. ansible-playbook \
+    -i tests/hostfile \
+    -e "role_name=ansible-gitlab-config" \
+    -e "gitlab_host=$GITLAB_HOST" \
+    -e "gitlab_user=$GITLAB_USER" \
+    -e "gitlab_password=$GITLAB_PASSWORD" \
+    -e "gitlab_token=$GITLAB_TOKEN" \
+    tests/gitlab-config.yml -v --syntax-check && \
+ANSIBLE_ROLES_PATH=$(pwd)/.. ansible-playbook \
+    -i tests/hostfile \
+    -e "role_name=ansible-gitlab-config" \
+    -e "gitlab_host=$GITLAB_HOST" \
+    -e "gitlab_user=$GITLAB_USER" \
+    -e "gitlab_password=$GITLAB_PASSWORD" \
+    -e "gitlab_token=$GITLAB_TOKEN" \
+    tests/gitlab-config.yml -v --list-tasks && \
 ANSIBLE_ROLES_PATH=$(pwd)/.. ansible-playbook \
     -i tests/hostfile \
     -e "role_name=ansible-gitlab-config" \
@@ -13,3 +35,4 @@ ANSIBLE_ROLES_PATH=$(pwd)/.. ansible-playbook \
     -e "gitlab_password=$GITLAB_PASSWORD" \
     -e "gitlab_token=$GITLAB_TOKEN" \
     tests/gitlab-config.yml -vv
+)
